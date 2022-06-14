a69d6af886fc037956ecca3d0a603e19ec7743f8
FilipKlominek <filip.klominek@educanet.cz>
2022-06-14 13:35:55 +0200

    commit

--- src/app/services/generator.ts
+++ src/app/services/generator.ts
@@ -14,25 +14,32 @@ export class Island {
       }
     }
 
+    /*
+        let midY = island.length / 2;
+        let midX = island[0].length / 2;
 
-    let midY = island.length / 2;
-    let midX = island[0].length / 2;
+        let pY = 0.0;
+        let pX = 0.0;
 
-    let pY = 0.0;
-    let pX = 0.0;
+        let probability = 0;
 
-    let probability = 0;
+        for (let y = 0; y < island.length; y++) {
+          for (let x = 0; x < island[y].length; x++) {
+            pY = Math.abs(midY - y) / midY;
+            pX = Math.abs(midX - x) / midX;
 
-    for (let y = 0; y < island.length; y++) {
-      for (let x = 0; x < island[y].length; x++) {
-        pY = Math.abs(midY - y) / midY;
-        pX = Math.abs(midX - x) / midX;
+            console.log(pY)
+            console.log(pX)
 
-        probability = pY * pX;
+            probability = pY * pX;
 
-        if (probability > Math.random()) island[y][x] = 1;
-      }
-    }
+            if (probability > Math.random()) island[y][x] = 1;
+          }
+        }
+    */
+
+
+    island[island.length / 2][island[0].length / 2] = 1;
 
 
     return island;
